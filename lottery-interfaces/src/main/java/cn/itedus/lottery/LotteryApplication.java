a9783e5a88ee13a5f9b17c8f47e43c6d6db1380f
zero <1315436708@qq.com>
2023-04-19 09:51:07 +0800

    搭建DDD四层架构，跑通广播模式rpc调用

---
+++ lottery-interfaces/src/main/java/cn/itedus/lottery/LotteryApplication.java
@@ -0,0 +1,18 @@
+package cn.itedus.lottery;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@Configurable
+@EnableDubbo
+public class LotteryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryApplication.class, args);
+    }
+
+}
